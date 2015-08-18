4225665c92f6842c3177257556eebcab3e0d7191
Josh <joshcrist20@gmail.com>
2015-08-18 08:33:30 -0600

    Add image upload with paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,8 @@ Rails.application.configure do
 
 config.action_mailer.default_url_options = { host: 'joshcristdev.com', port: 3000 }
 
+# Paperclip config:
+Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
 end
